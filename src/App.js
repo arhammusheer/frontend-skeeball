304b108ec41e0470c55e89f6f1c61c3045563e76
arhammusheer <arhammusheer@gmail.com>
2021-11-06 00:21:04 -0400

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import { io } from "socket.io-client";
 
-function App() {
+export default function App() {
+  const socket = io("http://skeeball.croissant.one:3001");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    "socket.io"
   );
 }
-
-export default App;
